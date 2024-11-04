f63141ae69a2822fb7ed447946f809c27404e8af
briankworld <129961010+briankworld@users.noreply.github.com>
2024-11-04 01:11:05 -0800

    Update slice-deletefunc.go

--- slice-deletefunc.go
+++ slice-deletefunc.go
@@ -1,19 +1,30 @@
 package main
 
-import {
-  "fmt"
-  "slice"
-}
+import (
+	"fmt"
+
+	"golang.org/x/exp/slices"
+)
 
 func main() {
-  arr := []int {1,2,3,4,5,6,7,8,9}
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-  arr2 := slice.DeleteFunc(arr, func (a int) bool {
-    if a % 2 == 0 {
-      return true
-    }
-    return false
-  })
+	arr2 := slices.Clone(arr)
+	arr2 = slices.DeleteFunc(arr2, func(a int) bool {
+		if a%2 == 0 {
+			return true
+		}
+		return false
+	})
 
-  fmt.Printf("Original: %v, Output %v", arr, arr2)
+	fmt.Println("Original slice:", arr)
+	fmt.Println("After DeleteFunc:", arr2)
+
+	target := 7
+	index, found := slices.BinarySearch(arr2, target)
+	if found {
+		fmt.Printf("Found %d at index %d\n", target, index)
+	} else {
+		fmt.Printf("%d not found. It would be at index %d\n", target, index)
+	}
 }
